a447410e05de49464441b2c7cd1216df1a2e6322
Norb Beaver <norb.beaver@gmail.com>
2014-01-24 18:54:36 +0200

    java docs

--- com.digiarea.es5/src/com/digiarea/es5/StringLiteralDouble.java
+++ com.digiarea.es5/src/com/digiarea/es5/StringLiteralDouble.java
@@ -1,6 +1,13 @@
-/*
- * 
- */
+/*******************************************************************************
+ * Copyright (c) 2011 - 2014 DigiArea, Inc. and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     DigiArea, Inc. - initial API and implementation
+ *******************************************************************************/
 package com.digiarea.es5;
 
 import com.digiarea.es5.StringLiteral;
